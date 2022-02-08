ff696f54eff9abc90a9b35e3e5ecbea480ca0c8b
francesco-abritta <fra.abritta@gmail.com>
2022-02-08 15:59:44 +0100

    bozza vuota header

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="header">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .header{
+    height: 70px;
+    background-color: #1F1F52;
+  }
+</style>
