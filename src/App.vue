1c421f65d7f6f40dc8c622f939afbb011e49f00d
francesco-abritta <fra.abritta@gmail.com>
2022-02-08 21:01:52 +0100

    creato e agganciato main

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <template>
   <div id="app">
     <Header />
+    <div class="contenitore">
+      <Main />
+    </div>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Main from './components/Main.vue'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Main
   }
 }
 </script>
 
 <style lang="scss">
   @import './assets/global.scss';
+  .cont{
+    margin: 0 auto;
+    max-width: 80%;
+  }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Main'
+}
+</script>
+
+<style scoped>
+    div{
+        margin-top: 100px;
+    }
+</style>
