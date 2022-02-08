2120ba4183b38760e66d0cff7786ce16eea8f4dc
francesco-abritta <fra.abritta@gmail.com>
2022-02-08 16:17:52 +0100

    img e prima bozza header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,24 @@
 <template>
   <div>
     <div class="header">
+      <div class="logo">
 
+      </div>
+      <div class="voices">
+        <ul>
+          <li>Ciao</li>
+          <li>Ciao</li>
+          <li>Ciao</li>
+          <li>Ciao</li>
+          <li>Ciao</li>
+          <li>Ciao</li>
+          <li>C</li>
+          <li>U</li>
+        </ul>
+      </div>
+      <div class="searchbar">
+        <input type="text">
+      </div>
     </div>
   </div>
 </template>
@@ -16,8 +33,42 @@ export default {
 </script>
 
 <style scoped lang="scss">
+  @import '../assets/vars.scss';
+
   .header{
     height: 70px;
-    background-color: #1F1F52;
+    background-color: $header-color;
+    color: $header-color-text;
+    display: flex;
+
+      .logo{
+
+      }
+
+      .voices{
+        background: blue;
+          ul{
+            list-style: none;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 70px;
+
+              li{
+                margin: 0 10px;
+              }
+          }
+      }
+
+      .searchbar{
+          display: flex;
+          align-items: center;
+          background: red;
+      }
+      
+
+
   }
+
+  
 </style>
